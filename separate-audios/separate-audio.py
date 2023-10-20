efb0b41bd7d4b54fec6c785b42c17c033236dcde
pansong291 <pansong291@sina.com>
2023-10-21 01:27:49 +0800

    feat: 人声分离

--- separate-audios/separate-audio.py
+++ separate-audios/separate-audio.py
@@ -10,16 +10,17 @@ files = os.listdir(directory)
 # 拼接相对路径并用空格隔开
 file_paths = " ".join([os.path.join(directory, file) for file in files])
 
-command = "ls -l" # "spleeter separate -o audio_output -f {foldername}/{filename}_{instrument}.{codec} " + file_paths
+command = "spleeter separate -o audio_output -f {foldername}/{filename}_{instrument}.{codec}"
 
 print(command)
+print(file_paths)
 
 # 设置环境变量
 env = {'GITHUB_REPOSITORY': 'deezer/spleeter'}
 
 # 执行Shell命令，获取输出结果
 try:
-    result = subprocess.run(command, shell=True, env=env, check=True, capture_output=True, text=True)
+    result = subprocess.run([command, file_paths], shell=True, env=env, check=True, capture_output=True, text=True)
     print("Command executed successfully.")
     print("Command output:\n", result.stdout)
 except subprocess.CalledProcessError as e:
