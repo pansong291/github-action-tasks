5d633e66ddcdc6260cdc6ab0bcb9889cbca76409
pansong291 <pansong291@sina.com>
2023-10-20 23:44:01 +0800

    feat: 人声分离

--- separate-audios/separate-audio.py
+++ separate-audios/separate-audio.py
@@ -10,8 +10,12 @@ files = os.listdir(directory)
 # 拼接相对路径并用空格隔开
 file_paths = " ".join([os.path.join(directory, file) for file in files])
 
+command = "spleeter separate -o audio_output -f {foldername}/{filename}_{instrument}.{codec} " + file_paths
+
+print(command)
+
 # 执行Shell命令，获取输出结果
-result = subprocess.run("spleeter separate \\\n -o audio_output \\\n -f {foldername}/{filename}_{instrument}.{codec} \\\n " + file_paths, shell=True, capture_output=True, text=True)
+result = subprocess.run(command, shell=True, capture_output=True, text=True)
 
 # 打印输出结果
 print(result.stdout)
